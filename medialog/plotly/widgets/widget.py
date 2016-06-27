0a4dd8b5ccd11558a61b84c389ab747f06d48236
Espen Moe-Nilssen <espen@medialog.no>
2016-06-28 01:21:18 +0200

    another try...

--- medialog/plotly/widgets/widget.py
+++ medialog/plotly/widgets/widget.py
@@ -1,23 +1,31 @@
+"""Table Widget Implementation
+
+$Id$
+"""
+__docformat__ = "reStructuredText"
 import zope.component
 import zope.interface
 import zope.schema.interfaces
 
 from z3c.form import interfaces
-from z3c.form import widget
-from z3c.form.browser import text
+from z3c.form.widget import Widget, FieldWidget
+from z3c.form.browser import widget
+
+@zope.interface.implementer_only(interfaces.ITextWidget)
+class TableWidget(widget.HTMLTextInputWidget, Widget):
+    """Input type text widget implementation."""
+
+    klass = u'table-widget'
+    css = u'table'
+    value = u''
+
+    def update(self):
+        super(TableWidget, self).update()
+        widget.addFieldClass(self)
 
 
-class ITableWidget(interfaces.IWidget):
-    """Table widget."""
-
-
-class TableWidget(text.TextWidget):
-    zope.interface.implementsOnly(ITableWidget)
-    
-    def javascript():  
-        return ""
-             
-
+@zope.component.adapter(zope.schema.interfaces.IField, interfaces.IFormLayer)
+@zope.interface.implementer(interfaces.IFieldWidget)
 def TableFieldWidget(field, request):
     """IFieldWidget factory for TableWidget."""
-    return widget.FieldWidget(field, TableWidget(request))
+    return FieldWidget(field, TableWidget(request))
